585698cbcea675e00f16cfaaa4431508da192720
FerZe <ferrefttrr@gmail.com>
2024-10-20 20:21:21 -0600

    templates

--- members/views.py
+++ members/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def members(request): #nombre de la vista "members"
-    return HttpResponse('Hello world!')
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
