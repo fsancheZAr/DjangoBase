a4752b2c1cc8b580dce18707514726b9f430a91b
FerZe <ferrefttrr@gmail.com>
2024-10-23 14:55:24 -0600

    display-data

--- members/views.py
+++ members/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from .models import Member
 
 def members(request): #nombre de la vista "members"
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all.members.html')
+
+    context = {
+        'mymembers': mymembers
+    }
+    return HttpResponse(template.render(context, request))
+
+def datails(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template,render(context, request))
