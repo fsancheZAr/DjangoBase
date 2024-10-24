63ff57f645acd753c3ff01d975ac6325f58ca4ed
FerZe <ferrefttrr@gmail.com>
2024-10-24 12:57:02 -0600

    Establecer Campos para Mostrar

--- members/admin.py
+++ members/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-
 from .models import Member
 
 # Register your models here.
-admin.site.register(Member)
+class MemberAdmin(admin.ModelAdmin): #se crea una función 
+    list_display = ("firstname", "lastname", "joined_date",) #detallamos que se va mostrar en usando list
+
+admin.site.register(Member, MemberAdmin) #se registra al modelo
--- members/models.py
+++ members/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
 
 #De forma predeterminada, todos los modelos se crearán como 
 #tablas en la base db.sqlite3
